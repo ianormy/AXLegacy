be0d62e8bb1adf568fcae68dc6c88cc3ff1d0178
Ian Ormesher <ianormy@gmail.com>
2021-07-24 15:38:10 +0100

    Initial version (#1)
    Initial version of the project.

---
+++ AxLegacy/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace AxLegacy.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("AxLegacy")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("AxLegacy")>]
+[<assembly: AssemblyCopyright("Copyright ©  2021")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("c63b546d-e33f-4518-9019-1e541714d227")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ AxLegacy/Program.fs
@@ -0,0 +1,7 @@
+﻿open AMovie
+
+[<EntryPoint>]
+let main argv = 
+    let a = ActiveMovieClass()
+    printfn "Current volume: %d" a.Volume
+    0 // return an integer exit code
